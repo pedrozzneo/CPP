1151a9c155749c7a08b87459ccdf9642bcaf6d27
Pedro_Neo <pedrozzneo@gmail.com>
2024-03-28 17:56:45 -0300

    Added description comment

--- car speed.cpp
+++ car speed.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//that code calculates the velocity also called speed of the car
 int main () {
 	float car_speed, displacement, time;
 	cout << "how long did the displace take? ";
